006df2e031a61386cb27ab95b5028f24a521cf73
Torrence Cole <trcole@gmail.com>
2021-02-21 10:39:51 -0500

    extracted versioning query to the ticket model

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -17,6 +17,10 @@ export interface TicketDoc extends mongoose.Document {
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  findByEvent(event: {
+    id: string;
+    version: number;
+  }): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema<TicketDoc>(
@@ -42,8 +46,15 @@ const ticketSchema = new mongoose.Schema<TicketDoc>(
 );
 
 ticketSchema.set('versionKey', 'version');
+// Lecture 379 has another implementation to not use this module
 ticketSchema.plugin(updateIfCurrentPlugin);
 
+ticketSchema.statics.findByEvent = (event: { id: string; version: number }) => {
+  return Ticket.findOne({
+    _id: event.id,
+    version: event.version - 1,
+  });
+};
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({
     _id: attrs.id,
