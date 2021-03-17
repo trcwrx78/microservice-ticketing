7d67e87348de5b17862d090b8c492e8f2463f945
Torrence Cole <trcole@gmail.com>
2021-03-17 11:07:00 -0400

    quick fix for build-client and ingress

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,7 @@ const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     // we are on the server!
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.ticketing-dev-app.xyz',
       headers: req.headers,
     });
   } else {
